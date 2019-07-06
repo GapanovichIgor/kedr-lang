55ac7a10c4b666621c59505cdaba59aebe3328af
igor gapanovich <gapanovich.igor@gmail.com>
2019-07-07 00:41:18 +0700

    parseEnc

--- Kedr.Lexer/Tokenizer.fs
+++ Kedr.Lexer/Tokenizer.fs
@@ -1,33 +1,29 @@
 module Kedr.Tokenizer
 
-open System
 open Kedr
 open System.IO
 open System.Text
 
 open System.Collections.Generic
-open Kedr
 open ParserComposition
 open ParserPrimitives
 open TokenParsers
 
-let parse (encoding : Encoding) (stream : Stream) : Token list =
-    let reader = new StreamReader(stream, encoding)
-    
-    let readChar () =
+let private skipWhitespace =
+    let whitespace = [ ' '; '\t' ] |> HashSet
+    skipWhile whitespace.Contains
+
+let private parseReader (reader: StreamReader) =
+    let readChar() =
         let i = reader.Read()
         if i = -1
         then None
-        else Some (char i)
-        
+        else Some(char i)
+
     let tape = Tape(readChar)
-    
-    let skipWhitespace =
-        let whitespace = [ ' '; '\t' ] |> HashSet
-        skipWhile whitespace.Contains
-    
+
     let parser =
-        zeroOrMore(
+        zeroOrMore (
             (skipWhitespace |> commitOnSuccess)
             >>.
             (chooseLongest [
@@ -41,9 +37,17 @@ let parse (encoding : Encoding) (stream : Stream) : Token list =
     match parser tape with
     | Ok ok -> ok.value
     | Error _ -> []
-    
-    
-let parseString (str : string) : Token list =
-    let encoding = Encoding.UTF8
-    let ms = new MemoryStream(encoding.GetBytes str)
-    parse encoding ms
+
+let parse (stream: Stream): Token list =
+    let reader = new StreamReader(stream)
+    parseReader reader
+
+let parseEnc (encoding: Encoding) (stream: Stream): Token list =
+    let reader = new StreamReader(stream, encoding)
+    parseReader reader
+
+let parseString (str: string): Token list =
+    let encoding = Encoding.Unicode
+    let bytes = encoding.GetBytes str
+    let ms = new MemoryStream(bytes)
+    parseEnc encoding ms
