98b72a7127833d3c891e553c89a0a3923a2b3f4c
igor gapanovich <gapanovich.igor@gmail.com>
2019-11-20 00:22:16 +0700

    Fixed AST function application

--- src/Kedr.AST/AST.fs
+++ src/Kedr.AST/AST.fs
@@ -5,6 +5,6 @@ type Identifier = string
 type ValueExpr =
     | StringLiteral of content : string
     | IdentifierRef of identifier : Identifier
-    | FunctionApplication of fnIdentifier : Identifier * arguments : ValueExpr list
+    | FunctionApplication of fn : ValueExpr * arg : ValueExpr
 
 type ValueBinding = ValueBinding of identifier : Identifier * value : ValueExpr 
