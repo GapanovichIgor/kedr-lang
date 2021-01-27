19ea04d09bdd66fd18e22412e6eba82464b74a6c
igor gapanovich <gapanovich.igor@gmail.com>
2021-01-27 20:45:18 +0700

    - Action table is now a normal Map; Reject is assumed when value is missing - Parse tables structured as maps to maps instead of maps of composite keys

--- src/utility/Kedr.ParserGenerator/ParsingTable.fs
+++ src/utility/Kedr.ParserGenerator/ParsingTable.fs
@@ -5,61 +5,65 @@ type internal Action<'a> =
     | Shift
     | Reduce of Production<'a>
     | Accept
-    | Reject
-
-type internal TableKey<'a when 'a : comparison> = {
-    state : State<'a>
-    symbol : 'a
-}
 
 type internal ParsingTable<'s when 's : comparison> = private {
     grammar : Grammar<'s>
-    goto : Map<TableKey<'s>, State<'s>>
-    action : DefaultingMap<TableKey<'s>, Action<'s>>
+    goto : Map<State<'s>, Map<'s, State<'s>>>
+    action : Map<State<'s>, Map<'s, Action<'s>>>
 }
 
 module internal ParsingTable =
     let create (automaton : Automaton<_>) =
-        let action : DefaultingMap<TableKey<'s>, Action<'s>> =
+        let action =
             seq {
                 for state in automaton.states do
                     let finalConfigurations = state.configurations |> Set.filter Configuration.isFinal
 
-                    for cfg in finalConfigurations do
-                        for lookahead in cfg.lookahead do
-                            let action =
-                                if cfg.production.from = automaton.grammar.startingSymbol
-                                then Accept
-                                else Reduce cfg.production
+                    let stateActions =
+                        seq {
+                            for cfg in finalConfigurations do
+                                for lookahead in cfg.lookahead do
+                                    let action =
+                                        if cfg.production.from = automaton.grammar.startingSymbol
+                                        then Accept
+                                        else Reduce cfg.production
 
-                            yield ({ state = state; symbol = lookahead }, action)
+                                    yield (lookahead, action)
 
-                    let symbolsWithTransitionFromState =
-                        automaton.transitions
-                        |> Seq.filter (fun tr -> tr.sourceState = state)
-                        |> Seq.map (fun tr -> tr.symbol)
-                        |> Seq.filter (automaton.grammar.terminals.Contains)
+                            let symbolsWithTransitionFromState =
+                                automaton.transitions
+                                |> Seq.filter (fun tr -> tr.sourceState = state)
+                                |> Seq.map (fun tr -> tr.symbol)
+                                |> Seq.filter (automaton.grammar.terminals.Contains)
 
-                    for symbol in symbolsWithTransitionFromState do
-                        yield ({ state = state; symbol = symbol }, Shift)
-            } |> DefaultingMap.ofSeq Reject
+                            for symbol in symbolsWithTransitionFromState do
+                                yield (symbol, Shift)
+                        } |> Map.ofSeq
+
+                    yield (state, stateActions)
+            } |> Map.ofSeq
 
         let goto =
             seq {
                 for state in automaton.states do
                     let nonFinalConfigurations = state.configurations |> Set.filter (not << Configuration.isFinal)
 
-                    for cfg in nonFinalConfigurations do
-                        match Configuration.getSymbolAfterCursor cfg with
-                        | None -> ()
-                        | Some symbol ->
-                            if automaton.grammar.nonTerminals.Contains(symbol) then
-                                let transitionOnSymbol =
-                                    automaton.transitions
-                                    |> Seq.filter (fun tr -> tr.sourceState = state && tr.symbol = symbol)
-                                    |> Seq.exactlyOne
+                    let stateGoto =
+                        seq {
+                            for cfg in nonFinalConfigurations do
+                                match Configuration.getSymbolAfterCursor cfg with
+                                | None -> ()
+                                | Some symbol ->
+                                    if automaton.grammar.nonTerminals.Contains(symbol) then
+                                        let transitionOnSymbol =
+                                            automaton.transitions
+                                            |> Seq.filter (fun tr -> tr.sourceState = state && tr.symbol = symbol)
+                                            |> Seq.exactlyOne
 
-                                yield ({ state = state; symbol = symbol }, transitionOnSymbol.destinationState)
+                                        yield (symbol, transitionOnSymbol.destinationState)
+                        } |> Map.ofSeq
+
+                    yield (state, stateGoto)
             } |> Map.ofSeq
 
         { grammar = automaton.grammar
